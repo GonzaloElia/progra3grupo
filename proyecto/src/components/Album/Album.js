8b24ab02cf24742405a540b4602e152cc2426206
ValenFP <vflorespueyo@udesa.edu.ar>
2022-09-16 18:06:00 -0300

    Album agregados

---
+++ proyecto/src/components/Album/Album.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+class album extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state ={
+          more: false,
+          favorite: false
+        }
+    }
+
+  more(){
+    if(this.state.more){
+      this.setState({
+        more: false
+      })
+    } else {
+      this.setState({
+        more: true
+      })
+    }
+  }
+  
+  addFavorites(id){
+    let favStorage = localStorage.getItem('favoritos')
+
+    if(favStorage === null){
+      let favArr = [id]
+      let arrToString = JSON.stringify(favArr)
+      localStorage.setItem('favoritos', arrToString)
+    } else {
+      let parsedArr = JSON.parse(favStorage)
+      parsedArr.push(id)
+      let arrToString = JSON.stringify(parsedArr)
+      localStorage.setItem('favoritos', arrToString)
+    }
+    this.setState({
+      favorite: true
+    })
+  }
+
+  removeFavorites(id){
+    let favStorage = localStorage.getItem('favoritos')
+    let parsedStorage = JSON.parse(favStorage)
+    let filterStorage = parsedStorage.filter(elm => elm !== id)
+
+    let storageToString = JSON.stringify(filterStorage)
+
+    localStorage.setItem('favoritos', storageToString)
+
+    this.setState({
+      favorite: false
+    })
+  }
+  render() {
+    return (
+      <div className="character-card">
+        <img src={this.props.info.cover}/>
+        <h3>{this.props.info.title}</h3>
+        
+        {
+          this.state.more ?
+          <p>Album: {this.props.info.title}</p>
+          :
+          ''
+        }
+        {
+          this.state.favorite ?
+            <button onClick={()=> this.removeFavorites(this.props.info.id)}>Sacar de favoritos</button>
+          :
+            <button onClick={()=> this.addFavorites(this.props.info.id)}>Añadir a favoritos</button>
+        }
+        
+        <button onClick={()=>this.more()}>Ver Más</button>
+        
+      </div>
+    )
+  }
+}
+export default album
